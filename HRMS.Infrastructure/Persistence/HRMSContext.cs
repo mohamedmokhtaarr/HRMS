6287308b4b4d37d9df421657462093a1e918eb3c
mohamed mokhtar <126124106+mohamedmokhtaarr@users.noreply.github.com>
2023-10-29 20:35:06 +0200

    Creating Project

---
+++ HRMS.Infrastructure/Persistence/HRMSContext.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace HRMS.Infrastructure.Persistence
+{
+   public class HRMSContext : DbContext
+    {
+
+
+
+        #region onConfig
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=HRMS;Trusted_Connection=True;TrustServerCertificate=True;");
+
+        }
+        #endregion
+
+
+
+    }
+}
